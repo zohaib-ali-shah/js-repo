68b6736ffa1b741c5334152a965f68369d8b0fad
zohaib-ali-shah <67370434+zohaib-ali-shah@users.noreply.github.com>
2024-05-10 18:53:27 +0000

    Learn about Numbers & Math Liberary

--- 0_basic/test.js
+++ 0_basic/test.js
@@ -1,2 +1,15 @@
 console.log("hello world")
 console.log("Hi Zohi Into the World of Github")
+
+const max = 20
+const min = 10
+
+console.log(Math.floor( Math.random() * (max - min +1) + min))
+
+// function getRandomInteger(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+//   }
+  
+//   const [min, max] = [10, 20];
+//   console.log(getRandomInteger(min, max));
+  
