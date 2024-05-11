989848dc42e4ce68985e3bf5d611b8313e283f20
zohaib-ali-shah <67370434+zohaib-ali-shah@users.noreply.github.com>
2024-05-11 11:10:16 +0000

    Learn about arrays methods slice and splice

---
+++ 01_basic/Arrays.js
@@ -0,0 +1,15 @@
+const myArr = [ 1 , 2 , 3 , 4 , 5 ];
+//console.log(myArr);
+const newArr = new Array(4 , 5, 6 , 7 , 8 , 9 )
+//console.log(newArr);
+
+//myArr.pop();
+//myArr.push(10)
+console.log(myArr);
+console.log(myArr.slice(1 , 4));
+console.log(myArr);
+console.log(myArr.splice(1, 4));
+console.log(myArr);
+
+
+
