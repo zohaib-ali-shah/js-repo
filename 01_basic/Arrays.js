5f092cdcc8e25fd48c20c4281a1434cf3779fdd2
zohaib-ali-shah <67370434+zohaib-ali-shah@users.noreply.github.com>
2024-06-26 12:30:12 +0000

    just comments

--- 01_basic/Arrays.js
+++ 01_basic/Arrays.js
@@ -4,6 +4,7 @@ const newArr = new Array(4 , 5, 6 , 7 , 8 , 9 )
 //console.log(newArr);
 
 //myArr.pop();
+<<<<<<< HEAD
 // //myArr.push(10)
 // console.log(myArr);
 // console.log(myArr.slice(1 , 4));
@@ -19,6 +20,15 @@ const finalNewArray = newArray.flat(3);
 console.log(finalNewArray);
 
 
+=======
+//myArr.push(10)
+console.log(myArr);
+console.log(myArr.slice(1 , 4));
+console.log(myArr);
+console.log(myArr.splice(1, 4));
+console.log(myArr);   
+//
+>>>>>>> eafd0bb (just comments)
 
 
 
