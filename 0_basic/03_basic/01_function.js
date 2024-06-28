e3d2d20966462a9f2989f86478287d59eabbcd43
zohaib-ali-shah <67370434+zohaib-ali-shah@users.noreply.github.com>
2024-06-28 08:00:08 +0000

    just created folder for fuction a little study it

---
+++ 0_basic/03_basic/01_function.js
@@ -0,0 +1,23 @@
+function MyName() {
+    console.log("ZOhaib")
+    console.log("ali")
+    console.log("shah")
+}
+
+//MyName();
+
+
+function addTwoNumber(number1, number2){
+
+    const result = (number1 + number2); 
+
+    return result
+}
+
+//console.log(addTwoNumber(1, 6));
+
+function userLoggin(userMessege = "Ali"){
+return `${userMessege} just logged in` 
+}
+console.log(userLoggin())
+
