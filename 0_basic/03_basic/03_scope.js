15dc13c2f76f840c8756e0e391a8c1d564a6227a
zohaib-ali-shah <67370434+zohaib-ali-shah@users.noreply.github.com>
2024-07-01 08:00:02 +0000

    variable scopes

---
+++ 0_basic/03_basic/03_scope.js
@@ -0,0 +1,27 @@
+if(true){
+    let z = 10
+    const x = 20
+    var y = 50    
+
+}
+
+// console.log(z)
+// console.log(x)
+//console.log(y)
+
+
+function zohaib () {
+    const fisrtName = "My First Name is 'zohaib'";
+
+    function ali () {
+
+        const secondName = "Ali"
+
+        console.log(`${fisrtName} and my second name is ${secondName}`)
+    }
+   
+    ali()
+
+// console.log(secondName)
+}
+zohaib()
